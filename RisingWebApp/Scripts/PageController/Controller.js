f956e396179183ffb7d7651d59802d42f93a8811
Jeff Guo <byguo2@yahoo.com>
2017-07-24 22:19:27 -0700

    Part 1

--- RisingWebApp/Scripts/PageController/Controller.js
+++ RisingWebApp/Scripts/PageController/Controller.js
@@ -1,4 +1,13 @@
 ﻿var app = angular.module('RisingWebApp');
 
 app.controller('HomeController', function ($scope, $rootScope, $location, $http) {
+    $scope.basic = {};
+    $scope.$watch("basic", function () {
+        if (!angular.isUndefined($scope.basic.apptype) && !angular.isUndefined($scope.basic.premises) && angular.isNumber($scope.basic.rent)
+            && angular.isDate($scope.basic.movein_date)) {
+            $('#basicDiv').removeClass("yellow_background");
+            $('#basicDiv').addClass("green_background");
+        }
+    }, true);
+
 });
